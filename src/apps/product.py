b7bb1d32ad76fc05b2006ef76304ae991c4be0ee
junkiYoon <juneki4933@gmail.com>
2022-04-13 18:46:57 +0900

    CREATE: product apps
    create crawl_product_image endpoint

---
+++ src/apps/product.py
@@ -0,0 +1,16 @@
+from fastapi import APIRouter, status, Depends
+
+from src import Settings, get_settings
+from src.utils.product import crawl_image_by_style_code
+
+
+router = APIRouter()
+
+
+@router.get("/product/image", status_code=status.HTTP_200_OK, tags=["product"])
+async def crawl_product_image(style_code: str, settings: Settings = Depends(get_settings)):
+    img_url = await crawl_image_by_style_code(style_code=style_code, settings=settings)
+
+    return {
+        'img_url': img_url
+    }
