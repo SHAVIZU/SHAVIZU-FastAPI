506b2d24f5085b13fd7f568930dd711b97ebab94
junkiYoon <juneki4933@gmail.com>
2022-04-27 14:53:00 +0900

    FIX: typing error

--- src/apps/shop.py
+++ src/apps/shop.py
@@ -12,7 +12,7 @@ router = APIRouter()
 
 @router.get("", status_code=status.HTTP_200_OK)
 async def shop_information(shop_id: int, settings: Settings = Depends(get_settings)):
-    with session_scope(settings) as session:
+    with session_scope(settings.MYSQL_DB_URL) as session:
         response = get_shop_information(session=session, shop_id=shop_id)
 
         return response
