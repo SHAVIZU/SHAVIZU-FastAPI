cb46eee5c06d9d986e898f45bb753a778e63743f
junkiYoon <juneki4933@gmail.com>
2022-05-02 11:39:39 +0900

    DELETE: async

--- src/apps/shop.py
+++ src/apps/shop.py
@@ -11,7 +11,7 @@ router = APIRouter()
 
 
 @router.get("", status_code=status.HTTP_200_OK)
-async def shop_information(shop_id: int, settings: Settings = Depends(get_settings)):
+def shop_information(shop_id: int, settings: Settings = Depends(get_settings)):
     with session_scope(settings.MYSQL_DB_URL) as session:
         response = get_shop_information(session=session, shop_id=shop_id)
 
