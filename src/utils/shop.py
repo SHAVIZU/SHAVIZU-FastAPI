3a32cd17c32d4b4d2450339588aa42c58fb587e4
junkiYoon <juneki4933@gmail.com>
2022-05-25 14:46:25 +0900

    UPDATE: add all shop functions
    get_shop_name, get_shop_items, get_shop_brands, get_shop_item_sizes

--- src/utils/shop.py
+++ src/utils/shop.py
@@ -1,9 +1,16 @@
-from sqlalchemy import literal_column
+from fastapi import HTTPException, status
+
+from sqlalchemy import literal_column, and_, func
 from sqlalchemy.orm import Session
 
 from src.core.models.shop import Tbl_shop
 from src.core.models.shop_information import Tbl_shop_information
 from src.core.models.shop_image import Tbl_shop_image
+from src.core.models.sell import Tbl_sell
+from src.core.models.item import Tbl_item
+from src.core.models.brand import Tbl_brand
+from src.core.models.item_size import Tbl_item_size
+from src.core.models.inventory import Tbl_inventory
 
 
 def get_shop_information(session: Session, shop_id: int):
@@ -28,4 +35,81 @@ def get_shop_information(session: Session, shop_id: int):
         "opening_hours": shop_info["opening_hours"],
         "description": shop_info["description"],
         "images": shop_info["image_urls"].split()
+    }
+
+
+def get_shop_name(session: Session, shop_id):
+    return session.query(Tbl_shop.name).filter(Tbl_shop.id == shop_id).first()["name"]
+
+
+def get_shop_items(session: Session, shop_id: int, brand: list, category: list, size: list, page: int):
+    limit = 30
+    offset = (page - 1) * limit
+
+    query = session.query(
+        Tbl_sell.discount_price,
+        Tbl_sell.discount_rate,
+        Tbl_item.name.label("item_name"),
+        Tbl_item.image_url,
+        Tbl_brand.name.label("brand_name"),
+        func.group_concat(func.concat(Tbl_item_size.size, ";", Tbl_inventory.amount), "|").label("stock")
+    )\
+        .join(Tbl_item, Tbl_sell.item_id == Tbl_item.id)\
+        .join(Tbl_brand, Tbl_item.brand_id == Tbl_brand.id)\
+        .join(Tbl_item_size, Tbl_item.id == Tbl_item_size.item_id)\
+        .join(Tbl_inventory, and_(Tbl_sell.id == Tbl_inventory.sell_id, Tbl_item_size.id == Tbl_inventory.item_size_id))\
+        .filter(Tbl_sell.shop_id == shop_id)
+
+    if not query.scalar():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="no such shop")
+
+    if not brand:
+        query = query.filter(Tbl_brand.name.in_(brand))
+    if not category:
+        query = query.filter(Tbl_item.category.in_(category))
+    if not size:
+        query = query.filter(Tbl_item_size.size.in_(size))
+
+    items = query.order_by(and_(Tbl_sell.created_at.desc(), Tbl_inventory.amount.desc()))\
+        .limit(limit).offset(offset).all()
+
+    return {
+        "shop_name": get_shop_name(session=session, shop_id=shop_id),
+        "items": [{
+            "discount_price": discount_price,
+            "discount_rate": discount_rate,
+            "item_name": item_name,
+            "image_url": image_url,
+            "brand_name": brand_name,
+            "stock": {
+                size_: amount for size_, amount in map(lambda x: x.split(";"), stock.split("|"))
+            }
+        } for discount_price, discount_rate, item_name, image_url, brand_name, stock in items]
+    }
+
+
+def get_shop_brands(session: Session, shop_id: int):
+    brands = session.query(Tbl_brand.name)\
+        .distinct(Tbl_brand.name)\
+        .join(Tbl_item, Tbl_brand.id == Tbl_item.brand_id)\
+        .join(Tbl_sell, Tbl_item.id == Tbl_sell.item_id)\
+        .filter(Tbl_sell.shop_id == shop_id).all()
+
+    return {
+        "brands": [brand["name"] for brand in brands]
+    }
+
+
+def get_shop_item_sizes(session: Session, shop_id: int, categories: list):
+    sizes = session.query(
+        Tbl_item_size.size
+    )\
+        .distinct(Tbl_item_size.size)\
+        .join(Tbl_item, Tbl_item_size.item_id == Tbl_item.id)\
+        .join(Tbl_sell, Tbl_item.id == Tbl_sell.item_id)\
+        .filter(and_(Tbl_sell.shop_id == shop_id, Tbl_item.category.in_(categories)))\
+        .all()
+
+    return {
+        "sizes": [size["size"] for size in sizes]
     }
