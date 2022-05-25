624140bc35e6f97d9cb907941ab31379ee509950
junkiYoon <juneki4933@gmail.com>
2022-05-25 14:45:27 +0900

    UPDATE: add all shop endpoints
    shop_items, shop_brands, shop_sizes

--- src/apps/shop.py
+++ src/apps/shop.py
@@ -1,10 +1,12 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, Query
+
+from typing import Optional, List
 
 from src import Settings, get_settings
 
 from src.core.models import session_scope
 
-from src.utils.shop import get_shop_information
+from src.utils.shop import get_shop_information, get_shop_items, get_shop_brands, get_shop_item_sizes
 
 
 router = APIRouter()
@@ -14,5 +16,47 @@ router = APIRouter()
 def shop_information(shop_id: int, settings: Settings = Depends(get_settings)):
     with session_scope(settings.MYSQL_DB_URL) as session:
         response = get_shop_information(session=session, shop_id=shop_id)
+
+        return response
+
+
+@router.get("/items", status_code=status.HTTP_200_OK)
+def shop_items(
+        shop_id: int,
+        page: int,
+        brand: Optional[List[int]] = Query(None),
+        category: Optional[List[str]] = Query(None),
+        size: Optional[List[str]] = Query(None),
+        settings: Settings = Depends(get_settings)
+):
+    with session_scope(settings.MYSQL_DB_URL) as session:
+        response = get_shop_items(
+            session=session,
+            shop_id=shop_id,
+            brand=brand,
+            category=category,
+            size=size,
+            page=page
+        )
+
+        return response
+
+
+@router.get("/brands", status_code=status.HTTP_200_OK)
+def shop_brands(shop_id: int, settings: Settings = Depends(get_settings)):
+    with session_scope(settings.MYSQL_DB_URL) as session:
+        response = get_shop_brands(session=session, shop_id=shop_id)
+
+        return response
+
+
+@router.get("/sizes", status_code=status.HTTP_200_OK)
+def get_sizes(
+        shop_id: int,
+        category: Optional[List[int]] = Query(None),
+        settings: Settings = Depends(get_settings)
+):
+    with session_scope(settings.MYSQL_DB_URL) as session:
+        response = get_shop_item_sizes(session=session, shop_id=shop_id, categories=category)
 
         return response
