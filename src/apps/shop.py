e0d3519cf47a94d61d22ff9ea46d8e29781691e5
junkiYoon <juneki4933@gmail.com>
2022-05-31 15:05:22 +0900

    FIX: get_sizes api's param

--- src/apps/shop.py
+++ src/apps/shop.py
@@ -53,7 +53,7 @@ def shop_brands(shop_id: int, settings: Settings = Depends(get_settings)):
 @router.get("/sizes", status_code=status.HTTP_200_OK)
 def get_sizes(
         shop_id: int,
-        category: Optional[List[int]] = Query(None),
+        category: Optional[List[str]] = Query(None),
         settings: Settings = Depends(get_settings)
 ):
     with session_scope(settings.MYSQL_DB_URL) as session:
