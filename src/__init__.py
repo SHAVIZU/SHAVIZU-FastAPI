946b8b409607112fa5c7605ee662324d93901070
junkiYoon <juneki4933@gmail.com>
2022-06-20 10:04:51 +0900

    UPDATE: Settings
    add chrome binary path

--- src/__init__.py
+++ src/__init__.py
@@ -6,6 +6,7 @@ from functools import lru_cache
 class Settings(BaseSettings):
     MYSQL_DB_URL: str
     CHROME_DRIVER_PATH: str
+    CHROME_BINARY_PATH: str
     AWS_ACCESS_KEY: str
     AWS_SECRET_KEY: str
 
